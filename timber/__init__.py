d32015178f24a7248eb0c9c90737cc91fd361cd0
Peter Downs <peterldowns@gmail.com>
2018-05-26 22:40:31 -0400

    Add tox testing.

--- timber/__init__.py
+++ timber/__init__.py
@@ -4,5 +4,6 @@ from __future__ import print_function, unicode_literals
 from .handler import TimberHandler, DEFAULT_CONTEXT
 from .helpers import TimberContext
 
+__version__ = '0.1.0'
 
 context = DEFAULT_CONTEXT
