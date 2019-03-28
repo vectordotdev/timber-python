9f1df93aa943546fc67f25ad626da279babaac3d
Mitchell Henke <mitchell@mitchellhenke.com>
2019-03-28 15:59:50 -0500

    update for version 2.0.0

--- timber/__init__.py
+++ timber/__init__.py
@@ -4,6 +4,6 @@ from __future__ import print_function, unicode_literals
 from .handler import TimberHandler, DEFAULT_CONTEXT
 from .helpers import TimberContext
 
-__version__ = '1.0.5'
+__version__ = '2.0.0'
 
 context = DEFAULT_CONTEXT
